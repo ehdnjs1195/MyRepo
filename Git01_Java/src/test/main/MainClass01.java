bb37494243dee985e811b1487b1efc8f93412b9d
ADW <ehdnjs1195@naver.com>
2019-12-30 11:03:13 +0900

    master 브랜치에서 Bye 출력

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -8,6 +8,6 @@ public class MainClass01 {
 		System.out.println("lab1 브렌치에서 작업함 2");
 		System.out.println("master 브랜치 Hello~");
 		System.out.println("lab1 브랜치에서 Hello~");
-
+		System.out.println("master 브랜치 Bye~");
 	}
 }
