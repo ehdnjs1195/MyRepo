45137afd89b55340b18dee563b37d88f34b3419b
ADW <ehdnjs1195@naver.com>
2019-12-30 10:10:06 +0900

    lab1 브렌치에서 작업함2

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -4,6 +4,7 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("kimgura");
 		System.out.println("monkey");
-		System.out.println("lab1 브렌치에 작업함");
+		System.out.println("lab1 브렌치에서 작업함");
+		System.out.println("lab1 브렌치에서 작업함 2");
 	}
 }
